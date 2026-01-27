e5f97e174d81821314ed4742007e3978e425bc17
Daniel Maynard <danielmaynard7@outlook.com>
2026-01-27 16:00:11 +0000

    Wrote a basic main function that takes URLs as program arguments, prefixes them with a protocol, makes a GET request to each one and prints the entire response.

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+)
 
+func main() {
+	for _, url := range os.Args[1:] {
+		// Adds an https:// prefix if the URL has no protocol
+		if !strings.HasPrefix(url, "http") {
+			url = "https://" + url
+		}
+
+		// Send request and print response
+		response, err := http.Get(url)
+		fmt.Printf("\n%v\nResponse: %v\nError: %v\n", url, response, err)
+	}
 }
